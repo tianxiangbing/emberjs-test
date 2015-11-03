4c2837db013e8bce535170395e2e59f7d41c18ad
tianxiangbing <55342775@qq.com>
2015-11-03 16:01:54 +0800

    componets test

--- example/test6/js/route.js
+++ example/test6/js/route.js
@@ -1,10 +1,12 @@
 Emberfest.Router.map(function() {
 	this.route('tickets');
-	this.route('talks',{path:'talks'});
-	this.resource('talks',  {
+	this.route('talks', {
+		path: 'talks'
+	});
+	this.resource('talks', {
 		path: '/talks'
-	},function() {
-	//这里要用resource,如果这里用route 的话，只会在第一次刷新时进TalksTalkRoute的model里.
+	}, function() {
+		//这里要用resource,如果这里用route 的话，只会在第一次刷新时进TalksTalkRoute的model里.
 		this.resource('talk', {
 			path: '/talk/:talk_id'
 		})
@@ -20,8 +22,8 @@ Emberfest.TalksRoute = Ember.Route.extend({
 	}
 });
 Emberfest.TalkRoute = Ember.Route.extend({
-	model:function(args){
+	model: function(args) {
 		console.log(args)
-		return Emberfest.Talk.find(args.talk_id);
+		return Emberfest.Talk.findAll()
 	}
 });
